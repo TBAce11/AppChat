dab1ee84e7a7894b51cf39d767fc913f449ae65f
TBAce11 <tbaghilas@gmail.com>
2023-10-18 13:32:54 -0400

    AuthenticationService - login & logout (1.C & 1.D)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { MatIconModule } from "@angular/material/icon";
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- frontend/src/app/login/authentication.service.ts
+++ frontend/src/app/login/authentication.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable } from "rxjs";
 import { UserCredentials } from "./model/user-credentials";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment"; // à re-vérifier
 
 @Injectable({
   providedIn: "root",
@@ -10,18 +12,36 @@ export class AuthenticationService {
 
   private username = new BehaviorSubject<string | null>(null);
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
     this.username.next(localStorage.getItem(AuthenticationService.KEY));
   }
 
-  login(userCredentials: UserCredentials) {
-    localStorage.setItem(AuthenticationService.KEY, userCredentials.username);
-    this.username.next(userCredentials.username);
+  async login(userCredentials: UserCredentials): Promise<void> {
+    try {
+      await this.httpClient.post(
+        `${environment.backendUrl}/auth/login`,
+        userCredentials,
+        {
+          withCredentials: true,
+        }
+      );
+      localStorage.setItem(AuthenticationService.KEY, userCredentials.username);
+      this.username.next(userCredentials.username);
+    } catch (err) {
+      throw new Error("Erreur de connexion: " + err);
+    }
   }
 
-  logout() {
-    localStorage.removeItem(AuthenticationService.KEY);
-    this.username.next(null);
+  async logout(): Promise<void> {
+    try {
+      await this.httpClient.post(`${environment.backendUrl}/auth/logout`, {
+        withCredentials: true,
+      });
+      localStorage.removeItem(AuthenticationService.KEY);
+      this.username.next(null);
+    } catch (err) {
+      throw new Error("Erreur de connexion: " + err);
+    }
   }
 
   getUsername(): Observable<string | null> {
