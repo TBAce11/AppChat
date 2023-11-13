94cf91dd6bcaf30d1264f26f9bdbd71f53e6b369
TBAce11 <tbaghilas@gmail.com>
2023-11-12 19:57:17 -0500

    Ajustement FileReaderService

--- frontend/src/app/chat/file-reader.service.ts
+++ frontend/src/app/chat/file-reader.service.ts
@@ -29,20 +29,4 @@ export class FileReaderService {
 
     return window.btoa(binary);
   }
-
-  base64ToBlob(base64: string, type: string): Promise<Blob> {
-    return new Promise((resolve, reject) => {
-      const byteCharacters = atob(base64);
-      const byteNumbers = new Array(byteCharacters.length);
-
-      for (let i = 0; i < byteCharacters.length; i++) {
-        byteNumbers[i] = byteCharacters.charCodeAt(i);
-      }
-
-      const byteArray = new Uint8Array(byteNumbers);
-      const blob = new Blob([byteArray], { type: type });
-
-      resolve(blob);
-    });
-  }
 }
