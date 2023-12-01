5993a9b309858f9be2ec6a43919f61d8d6f4e564
TBAce11 <tbaghilas@gmail.com>
2023-11-30 19:46:15 -0500

    Frontend - Session expirée + Déconnexion websocket (1.E + 1.F) -> Messages manqués à ajouter

--- frontend/src/app/chat/messages.service.ts
+++ frontend/src/app/chat/messages.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable, firstValueFrom } from "rxjs";
+import { BehaviorSubject, Observable, firstValueFrom, throwError } from "rxjs";
 import { Message, NewMessageRequest } from "./message.model";
-import { HttpClient, HttpParams } from "@angular/common/http";
+import {
+  HttpClient,
+  HttpParams,
+  HttpErrorResponse,
+} from "@angular/common/http";
 import { environment } from "src/environments/environment";
+import { AuthenticationService } from "../login/authentication.service";
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: "root",
@@ -10,40 +16,64 @@ import { environment } from "src/environments/environment";
 export class MessagesService {
   private messages = new BehaviorSubject<Message[]>([]);
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {}
 
   async postMessage(message: NewMessageRequest): Promise<Message> {
-    return firstValueFrom(
-      this.httpClient.post<Message>(
-        `${environment.backendUrl}/messages`,
-        message,
-        {
-          withCredentials: true,
-        }
-      )
-    );
+    try {
+      return firstValueFrom(
+        this.httpClient.post<Message>(
+          `${environment.backendUrl}/messages`,
+          message,
+          {
+            withCredentials: true,
+          }
+        )
+      );
+    } catch (error) {
+      this.handleHttpError(error);
+      throw error;
+    }
   }
 
   async fetchMessages() {
-    const lastMessageId =
-      this.messages.value.length > 0
-        ? this.messages.value[this.messages.value.length - 1].id
-        : null;
+    try {
+      const lastMessageId =
+        this.messages.value.length > 0
+          ? this.messages.value[this.messages.value.length - 1].id
+          : null;
 
-    const isIncrementalFetch = lastMessageId != null;
-    let queryParameters = isIncrementalFetch
-      ? new HttpParams().set("fromId", lastMessageId)
-      : new HttpParams();
+      const isIncrementalFetch = lastMessageId != null;
+      let queryParameters = isIncrementalFetch
+        ? new HttpParams().set("fromId", lastMessageId)
+        : new HttpParams();
 
-    const messages = await firstValueFrom(
-      this.httpClient.get<Message[]>(`${environment.backendUrl}/messages`, {
-        params: queryParameters,
-        withCredentials: true,
-      })
-    );
-    this.messages.next(
-      isIncrementalFetch ? [...this.messages.value, ...messages] : messages
-    );
+      const messages = await firstValueFrom(
+        this.httpClient.get<Message[]>(`${environment.backendUrl}/messages`, {
+          params: queryParameters,
+          withCredentials: true,
+        })
+      );
+      this.messages.next(
+        isIncrementalFetch ? [...this.messages.value, ...messages] : messages
+      );
+    } catch (error) {
+      this.handleHttpError(error);
+      throw error;
+    }
+  }
+
+  private handleHttpError(error: any): void {
+    if (error instanceof HttpErrorResponse && error.status === 403) {
+      console.error("Erreur HTTP 403. Déconnexion en cours...");
+
+      this.clear();
+      this.authenticationService.logout();
+      this.router.navigate(["/login"]);
+    }
   }
 
   getMessages(): Observable<Message[]> {
--- frontend/src/app/chat/websocket.service.ts
+++ frontend/src/app/chat/websocket.service.ts
@@ -9,18 +9,46 @@ export type WebSocketEvent = "notif";
 })
 export class WebSocketService {
   private ws: WebSocket | null = null;
+  private retryInterval = 2000;
+  private retryAttempts = 10;
+
+  private events = new Subject<WebSocketEvent>();
 
   constructor() {}
 
   public connect(): Observable<WebSocketEvent> {
+    this.retryConnection();
+    return this.events.asObservable();
+  }
+
+  private retryConnection(attempt = 1) {
     this.ws = new WebSocket(`${environment.wsUrl}/notifications`);
-    const events = new Subject<WebSocketEvent>();
 
-    this.ws.onmessage = () => events.next("notif");
-    this.ws.onclose = () => events.complete();
-    this.ws.onerror = () => events.error("error");
+    this.ws.onopen = () => {
+      attempt = 1;
+      console.log("Connexion établie");
 
-    return events.asObservable();
+      // Notification de connexion établie
+      this.events.next("notif");
+    };
+
+    this.ws.onmessage = () => this.events.next("notif");
+
+    this.ws.onclose = () => {
+      if (attempt <= this.retryAttempts) {
+        console.log("Nouvelle tentative de connexion");
+        // Nouvelle tentative de connexion après l'intervalle spécifiée
+        setTimeout(() => this.retryConnection(attempt + 1), this.retryInterval);
+      } else {
+        // Maximum de tentatives atteint -> arrêt des tentatives et notification de l'erreur
+        console.log("Maximum de tentatives atteint");
+        this.events.complete();
+      }
+    };
+    this.ws.onerror = () => {
+      console.log("Erreur: perte de connexion en cours");
+      this.events.error("error");
+    };
   }
 
   public disconnect() {
